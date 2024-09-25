e9339ab1bf18b1bebabcd8c55fa26919b007ee33
alkaz07 <alkaz07@yandex.ru>
2024-09-25 16:18:32 +0300

    +сортировка

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,18 @@ public class Main {
     }
 
     private static void example6() {
+        List<Triangle> triangleList = new ArrayList<>();
+        triangleList.add(new Triangle(3, 4, 5));
+        triangleList.add(new Triangle(10, 10, 10));
+        triangleList.add(0, new Triangle(11, 12, 13));
+        triangleList.add(new Triangle(4, 4, 3.8));
+        System.out.println(triangleList);
+
+        triangleList.sort(new TriangleAreaComparator());
+
+        System.out.println(triangleList);
+
+        triangleList.sort((x, y) -> Double.compare(x.perimeter(), y.perimeter()));
 
 
     }
@@ -19,7 +31,7 @@ public class Main {
         triangleList.add(new Triangle(10, 10, 10));
         triangleList.add(0, new Triangle(11, 12, 13));
         System.out.println(triangleList);
-        triangleList.stream().forEach(x -> System.out.println(x+"\n площадь = "+x.area()+" периметр = "+x.perimeter()));
+        triangleList.stream().forEach(x -> System.out.println(x + "\n площадь = " + x.area() + " периметр = " + x.perimeter()));
 
     }
 
@@ -36,31 +48,38 @@ public class Main {
     }
 
 
-private static void example3() {
-    List<Triangle> triangleList = new ArrayList<>();
-    triangleList.add(new Triangle(3, 4, 5));
-    triangleList.add(new Triangle(10, 10, 10));
-    triangleList.add(0, new Triangle(11, 12, 13));
-    System.out.println(triangleList);
-    for (int i = 0; i < triangleList.size(); i++) {
-        System.out.println("очередной элемент списка: " + triangleList.get(i));
-        System.out.println("площадь очередного элемента списка: " + triangleList.get(i).area());
+    private static void example3() {
+        List<Triangle> triangleList = new ArrayList<>();
+        triangleList.add(new Triangle(3, 4, 5));
+        triangleList.add(new Triangle(10, 10, 10));
+        triangleList.add(0, new Triangle(11, 12, 13));
+        System.out.println(triangleList);
+        for (int i = 0; i < triangleList.size(); i++) {
+            System.out.println("очередной элемент списка: " + triangleList.get(i));
+            System.out.println("площадь очередного элемента списка: " + triangleList.get(i).area());
+        }
+    }
+
+    private static void example2() {
+        List<Triangle> triangleList = new ArrayList<>();
+        triangleList.add(new Triangle(3, 4, 5));
+        triangleList.add(new Triangle(10, 10, 10));
+        triangleList.add(0, new Triangle(11, 12, 13));
+        System.out.println(triangleList);
+    }
+
+    private static void example1() {
+        List<String> words = new ArrayList<>();
+        words.add("Привет");
+        words.add("мир");
+        words.add(0, "ква");
+        System.out.println(words);
     }
 }
 
-private static void example2() {
-    List<Triangle> triangleList = new ArrayList<>();
-    triangleList.add(new Triangle(3, 4, 5));
-    triangleList.add(new Triangle(10, 10, 10));
-    triangleList.add(0, new Triangle(11, 12, 13));
-    System.out.println(triangleList);
-}
-
-private static void example1() {
-    List<String> words = new ArrayList<>();
-    words.add("Привет");
-    words.add("мир");
-    words.add(0, "ква");
-    System.out.println(words);
-}
+class TriangleAreaComparator implements Comparator<Triangle>{
+    @Override
+    public int compare(Triangle t1, Triangle t2) {
+        return Double.compare(t1.area(), t2.area());
+    }
 }
